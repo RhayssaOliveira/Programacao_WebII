bce1be165fe7aaee6e2a8c5a338975a7a34c3777
RhayssaOliveira <rhayssaosantos05@icloud.com>
2024-04-04 14:28:35 -0300

    Exercício feito em 04/04/2024

---
+++ calculadora_simples/resposta.php
@@ -0,0 +1,19 @@
+<?php
+ 
+ $numero1 = $_POST['numero1'];
+ $numero2 = $_POST['numero2'];
+ $operacao = $_POST['operacao'];
+
+ if($operacao == 'soma'){
+    $resultado = $numero1 + $numero2;
+ }elseif($operacao == 'subtracao'){
+    $resultado = $numero1 - $numero2;
+ }elseif($operacao == 'multiplicacao'){
+    $resultado = $numero1 * $numero2;
+ }elseif($operacao == 'divisao'){
+    $resultado = $numero1 / $numero2;
+ }
+
+ echo "O resy=ultado da operação é: $resultado";
+ 
+ ?>
